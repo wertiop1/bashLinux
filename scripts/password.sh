7851f56699a216ff72790a787e5f8e3ca0eb02b3
wertiop1 <102436461+wertiop1@users.noreply.github.com>
2025-10-14 23:34:30 -0700

    im really bad at variables

--- scripts/password.sh
+++ scripts/password.sh
@@ -15,13 +15,13 @@ read -r  PSAGE
 if [ "$PSAGE" = "y" ];then
     echo "setting passwords to reset after 30 days"
     PASSMAX="$(grep -n 'PASS_MAX_DAYS' /etc/login.defs | grep -v '#' | cut -f1 -d:)"
-    sed -e "${PASSMAX}s/.*/PASS_MAX_DAYS	30/" /etc/login.defs > ${USER_HOME}/backup/temp1.txt
+    sed -e "${PASSMAX}s/.*/PASS_MAX_DAYS	30/" /etc/login.defs > $USER_HOME/backup/temp1.txt
     PASSMIN="$(grep -n 'PASS_MIN_DAYS' /etc/login.defs | grep -v '#' | cut -f1 -d:)"
-    sed -e "${PASSMIN}s/.*/PASS_MIN_DAYS	7/" /var/local/temp1.txt > ${USER_HOME}/backup/temp2.txt
+    sed -e "${PASSMIN}s/.*/PASS_MIN_DAYS	7/" /var/local/temp1.txt > $USER_HOME/backup/temp2.txt
     PASSWARN="$(grep -n 'PASS_WARN_AGE' /etc/login.defs | grep -v '#' | cut -f1 -d:)"
-    sed -e "${PASSWARN}s/.*/PASS_WARN_AGE	7/" /var/local/temp2.txt > ${USER_HOME}/backup/temp3.txt
-    mv /etc/login.defs ${USER_HOME}/backup/login.defs.old
-    mv ${USER_HOME}/backup/temp3.txt /etc/login.defs
-    rm ${USER_HOME}/backup/temp1.txt ~/Desktop/backup/temp2.txt
+    sed -e "${PASSWARN}s/.*/PASS_WARN_AGE	7/" /var/local/temp2.txt > $USER_HOME/backup/temp3.txt
+    mv /etc/login.defs $USER_HOME/backup/login.defs.old
+    mv $USER_HOME/backup/temp3.txt /etc/login.defs
+    rm $USER_HOME/backup/temp1.txt ~/Desktop/backup/temp2.txt
 fi
 
